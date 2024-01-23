af36da8d6bc53c9b5fbb1ece6fe62e9f1467e9d6
Kevin Schiroo <kjschiroo@gmail.com>
2024-01-22 21:51:55 -0600

    Add pauses for departures

--- bbs/metrotransit/__init__.py
+++ bbs/metrotransit/__init__.py
@@ -6,26 +6,38 @@ from playwright import sync_api
 from . import display
 
 
-_STATE = {"demo_departures": 60 * 5 + 75}
+_STATE = {"demo_departures": 60 * 9}
+
+
+def _highlight_departure_time(page):
+    page.query_selector_all(".depart-time")[0].evaluate(
+        # Highlight just the text part of the first departure time
+        """(element) => {
+            element.style.backgroundColor = "yellow";
+        }"""
+    )
 
 
 def get_departures(headless=True):
     with sync_api.sync_playwright() as p:
         browser = p.chromium.launch(headless=headless)
         page = browser.new_page()
+        headless or input("Ready to go to the page...")
         page.goto(f"https://www.metrotransit.org/nextrip/902/1/SNUN")
         page.wait_for_selector(".depart-time", state="attached")
+        headless or input("Find the departure time...")
+        _highlight_departure_time(page)
         departures = page.query_selector_all(".depart-time")
         times = [d.text_content() for d in departures]
-        headless or input("Press enter to continue...")
+        headless or input("Use it...")
         return [int(t.split(" ")[0]) for t in times if "Min" in t]
 
 
 def demo_departures(*args, **kwargs):
-    _STATE["demo_departures"] -= 1
+    _STATE["demo_departures"] -= 15
     if _STATE["demo_departures"] <= 0:
         _STATE["demo_departures"] = 60 * 10
-    return [_STATE["demo_departures"] // 60]
+    return [max(_STATE["demo_departures"] // 60, 1)]
 
 
 def select_departure(departures) -> int:
